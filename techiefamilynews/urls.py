d41adb6397f69ab1df7547cc059d585210feaedf
c3zh <chrissiepollock@gmail.com>
2013-07-08 18:26:47 -0400

    Fixed url problem

--- techiefamilynews/urls.py
+++ techiefamilynews/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from blogengine.views import PostsFeed
+from django.views.generic import ListView
+from blogengine.models import Category, Post
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,25 +19,27 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # Home page
-    url(r'^$', 'blogengine.views.getPosts'),
-    url(r'^(?P<selected_page>\d+)/?$', 'blogengine.views.getPosts'),
+    url(r'^(?P<page>\d+)?/?$', ListView.as_view(
+        model=Post,
+        paginate_by=5,
+        )),
 
     # Blog posts
-    url(r'^\d{4}/\d{1,2}/(?P[-a-zA-Z0-9]+)/?$', 'blogengine.views.getPost'),
+    url(r'^\d{4}/\d{1,2}/(?P<postSlug>[-a-zA-Z0-9]+)/?$', 'blogengine.views.getPost'),
 
     # Categories
+    url(r'^categories/?$', ListView.as_view(
+        model=Category,
+        )),
     url(r'^categories/(?P<categorySlug>\w+)/?$', 'blogengine.views.getCategory'),
     url(r'^categories/(?P<categorySlug>\w+)/(?P<selected_page>\d+)/?$', 'blogengine.views.getCategory'),
 
     # Comments
-	url(r'^comments/', include('django.contrib.comments.urls')),
-	
-	# RSS feeds
-	url(r'^feeds/posts/$', PostsFeed()),
-	
-	# RSS comments
-	url(r'^feeds/comments/$', PostsFeed()),
-	
-	# Flat pages
+    url(r'^comments/', include('django.contrib.comments.urls')),
+
+    # RSS feeds
+    url(r'^feeds/posts/$', PostsFeed()),
+
+    # Flat pages
     url(r'', include('django.contrib.flatpages.urls')),
 )
