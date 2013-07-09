577907aa26499cdcb1a2aa707e54fd978a8f4ed0
c3zh <chrissiepollock@gmail.com>
2013-07-09 09:49:20 -0400

    working on header

--- techiefamilynews/urls.py
+++ techiefamilynews/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from blogengine.views import PostsFeed
 from django.views.generic import ListView
 from blogengine.models import Category, Post
